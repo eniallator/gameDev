2ae47aebd48e924905256a3c01d3f2b1b8f4bb8d
eniallator <eniallatormc@gmail.com>
2016-05-10 19:09:05 +0100

    Added browserify

--- characterReducer.js
+++ characterReducer.js
@@ -1,75 +1,72 @@
-(function() {
+const initialState = {
+  position : {
+    x : 200,
+    y : 350
+  },
+  velocity : {
+    x : 0,
+    y : 0
+  },
+  radius : 50,
+  onGround : false
+};
 
-  const initialState = {
-    position : {
-      x : 200,
-      y : 350
-    },
-    velocity : {
-      x : 0,
-      y : 0
-    },
-    radius : 50,
-    onGround : false
-  };
+var movementSpeed = 0.2;
+var jumpPower = -20;
+var gravity = 0.09;
+var drag = 0.97;
 
-  var movementSpeed = 0.2;
-  var jumpPower = -10;
-  var gravity = 0.09;
-  var drag = 0.97;
+module.exports = function(state) {
+  if(state.playerCircle === undefined) {
+      state.playerCircle = initialState;
+      return state;
+  }
 
-  BOUNCE_GAME.characterReducer = function(state) {
-    if(state.playerCircle === undefined) {
-        state.playerCircle = initialState;
-        return state;
-    }
+  if(state.keysPressed.w && state.playerCircle.onGround){
+    state.playerCircle.velocity.y = jumpPower;
+  }
 
-    if(state.keysPressed.w && state.playerCircle.onGround){
-      state.playerCircle.velocity.y = jumpPower;
-    }
+  if(state.keysPressed.d){
+    state.playerCircle.velocity.x += movementSpeed;
+  }
 
-    if(state.keysPressed.d){
-      state.playerCircle.velocity.x += movementSpeed;
-    }
+  if(state.keysPressed.a){
+    state.playerCircle.velocity.x -= movementSpeed;
+  }
 
-    if(state.keysPressed.a){
-      state.playerCircle.velocity.x -= movementSpeed;
-    }
+  state.playerCircle.velocity.x *= drag;
+  state.playerCircle.velocity.y *= drag;
+  state.playerCircle.velocity.y += gravity;
 
-    state.playerCircle.velocity.x *= drag;
-    state.playerCircle.velocity.y *= drag;
-    state.playerCircle.velocity.y += gravity;
+  if (state.playerCircle.velocity.x > 0 && state.playerCircle.position.x + state.playerCircle.velocity.x < (state.environment.screenDimensions.dx - state.playerCircle.radius)) {
+    state.playerCircle.position.x += state.playerCircle.velocity.x;
+  } else if (state.playerCircle.position.x + state.playerCircle.velocity.x > (state.environment.screenDimensions.dx - state.playerCircle.radius)) {
+    state.playerCircle.velocity.x = -1 * state.playerCircle.velocity.x;
+  }
 
-    if (state.playerCircle.velocity.x > 0 && state.playerCircle.position.x + state.playerCircle.velocity.x < (state.environment.screenDimensions.dx - state.playerCircle.radius)) {
-      state.playerCircle.position.x += state.playerCircle.velocity.x;
-    } else if (state.playerCircle.position.x + state.playerCircle.velocity.x > (state.environment.screenDimensions.dx - state.playerCircle.radius)) {
-      state.playerCircle.velocity.x = -1 * state.playerCircle.velocity.x;
-    }
+  if (state.playerCircle.velocity.x < 0 && state.playerCircle.position.x + state.playerCircle.velocity.x > (state.environment.screenDimensions.x + state.playerCircle.radius)) {
+    state.playerCircle.position.x += state.playerCircle.velocity.x;
+  } else if (state.playerCircle.position.x + state.playerCircle.velocity.x < (state.environment.screenDimensions.x + state.playerCircle.radius)) {
+    state.playerCircle.velocity.x = -1 * state.playerCircle.velocity.x;
+  }
 
-    if (state.playerCircle.velocity.x < 0 && state.playerCircle.position.x + state.playerCircle.velocity.x > (state.environment.screenDimensions.x + state.playerCircle.radius)) {
-      state.playerCircle.position.x += state.playerCircle.velocity.x;
-    } else if (state.playerCircle.position.x + state.playerCircle.velocity.x < (state.environment.screenDimensions.x + state.playerCircle.radius)) {
-      state.playerCircle.velocity.x = -1 * state.playerCircle.velocity.x;
-    }
+  if (state.playerCircle.velocity.y > 0 && state.playerCircle.position.y + state.playerCircle.velocity.y < (state.environment.screenDimensions.dy - state.playerCircle.radius)) {
+    state.playerCircle.position.y += state.playerCircle.velocity.y;
+  } else if (state.playerCircle.position.y + state.playerCircle.velocity.y > (state.environment.screenDimensions.dy - state.playerCircle.radius)) {
+    state.playerCircle.velocity.y = -1 * state.playerCircle.velocity.y;
+  }
 
-    if (state.playerCircle.velocity.y > 0 && state.playerCircle.position.y + state.playerCircle.velocity.y < (state.environment.screenDimensions.dy - state.playerCircle.radius)) {
-      state.playerCircle.position.y += state.playerCircle.velocity.y;
-    } else if (state.playerCircle.position.y + state.playerCircle.velocity.y > (state.environment.screenDimensions.dy - state.playerCircle.radius)) {
-      state.playerCircle.velocity.y = -1 * state.playerCircle.velocity.y;
-    }
+  if (state.playerCircle.position.y + state.playerCircle.velocity.y > (state.environment.screenDimensions.y + state.playerCircle.radius)) {
+    state.playerCircle.position.y += state.playerCircle.velocity.y;
+  } else {
+    state.playerCircle.velocity.y = -1 * state.playerCircle.velocity.y;
+  }
 
-    if (state.playerCircle.position.y + state.playerCircle.velocity.y > (state.environment.screenDimensions.y + state.playerCircle.radius)) {
-      state.playerCircle.position.y += state.playerCircle.velocity.y;
-    } else {
-      state.playerCircle.velocity.y = -1 * state.playerCircle.velocity.y;
-    }
+  if (state.playerCircle.position.y > (state.environment.screenDimensions.dy - state.playerCircle.radius - 1)) {
+    state.playerCircle.onGround = true;
+  } else {
+    state.playerCircle.onGround = false;
+  }
 
-    if (state.playerCircle.position.y > (state.environment.screenDimensions.dy - state.playerCircle.radius - 1)) {
-      state.playerCircle.onGround = true;
-    } else {
-      state.playerCircle.onGround = false;
-    }
-
-    return state;
-  };
-})();
+  return state;
+};
--- gameEngine.js
+++ gameEngine.js
@@ -1,110 +1,110 @@
-(function() {
-  var canvas = document.getElementById("gameWorld");
-  var ctx = canvas.getContext("2d");
-  var canvasDiv = document.getElementsByClassName("canvasContainer")[0];
-  var smallCirclePosition = {
-    x : -50,
-    y : -50
-  };
+var characterReducer = require('./characterReducer');
 
-  let state = {
-    keysPressed : {
-      w : false,
-      a : false,
-      d : false
-    },
-    environment : {
-      screenDimensions : {
-        x : 0,
-        y : 0,
-        dx : canvasDiv.offsetWidth,
-        dy : canvasDiv.offsetHeight
-      }
-    }
-  };
+var canvas = document.getElementById("gameWorld");
+var ctx = canvas.getContext("2d");
+var canvasDiv = document.getElementsByClassName("canvasContainer")[0];
+var smallCirclePosition = {
+  x : -50,
+  y : -50
+};
 
-  var score = 0;
-
-  function resizeWindow(){
-    canvas.width = canvasDiv.offsetWidth;
-    canvas.height = canvasDiv.offsetHeight;
-  }
-
-  function renderScore(){
-    var outputScore = document.getElementById("outputScore");
-    outputScore.value = score;
-  }
-
-  function tick(){
-    const screenDimensions = state.environment.screenDimensions;
-    state = BOUNCE_GAME.characterReducer(state);
-    const playerPosition = state.playerCircle.position;
-    updateScore();
-
-    ctx.clearRect(screenDimensions.x,screenDimensions.y,screenDimensions.dx,screenDimensions.dy);
-    ctx.beginPath();
-    ctx.arc(playerPosition.x,playerPosition.y,state.playerCircle.radius,0,Math.PI*2,true);
-    ctx.arc(smallCirclePosition.x,smallCirclePosition.y,10,0,Math.PI*2,true);
-    ctx.fill();
-
-    requestAnimationFrame(tick);
-  }
-
-  renderScore()
-  resizeWindow()
-  tick()
-
-  function updateScore(){
-
-    const xDiff = Math.abs(state.playerCircle.position.x - smallCirclePosition.x);
-    const yDiff = Math.abs(state.playerCircle.position.y - smallCirclePosition.y);
-
-    if (Math.sqrt((xDiff^2) + (yDiff^2)) - 10 - state.playerCircle.radius < 0){ // If to catch if the big circle has gotten the small circle
-      score += 1;
-
-      smallCirclePosition = {
-        x : -50,
-        y : -50
-      };
-
-      renderScore();
+let state = {
+  keysPressed : {
+    w : false,
+    a : false,
+    d : false
+  },
+  environment : {
+    screenDimensions : {
+      x : 0,
+      y : 0,
+      dx : canvasDiv.offsetWidth,
+      dy : canvasDiv.offsetHeight
     }
   }
+};
 
-  window.addEventListener("keydown",function(event){
-    switch (event.code) {
-      case "KeyW":
+var score = 0;
 
-        state.keysPressed.w = true;
-        break;
-      case "KeyD":
+function resizeWindow(){
+  canvas.width = canvasDiv.offsetWidth;
+  canvas.height = canvasDiv.offsetHeight;
+}
 
-        state.keysPressed.d = true;
-        break;
-      case "KeyA":
+function renderScore(){
+  var outputScore = document.getElementById("outputScore");
+  outputScore.value = score;
+}
 
-        state.keysPressed.a = true;
-    }
-  });
+function tick(){
+  const screenDimensions = state.environment.screenDimensions;
+  state = characterReducer(state);
+  const playerPosition = state.playerCircle.position;
+  updateScore();
 
-  window.addEventListener("keyup",function(event){
-    switch (event.code) {
-      case "KeyW":
+  ctx.clearRect(screenDimensions.x,screenDimensions.y,screenDimensions.dx,screenDimensions.dy);
+  ctx.beginPath();
+  ctx.arc(playerPosition.x,playerPosition.y,state.playerCircle.radius,0,Math.PI*2,true);
+  ctx.arc(smallCirclePosition.x,smallCirclePosition.y,10,0,Math.PI*2,true);
+  ctx.fill();
 
-        state.keysPressed.w = false;
-        break;
-      case "KeyD":
+  requestAnimationFrame(tick);
+}
 
-        state.keysPressed.d = false;
-        break;
-      case "KeyA":
+renderScore()
+resizeWindow()
+tick()
 
-        state.keysPressed.a = false;
-    }
-  });
+function updateScore(){
 
-  window.addEventListener("click",function(event){
-    smallCirclePosition.x = event.clientX - 10
-    smallCirclePosition.y = event.clientY - 25
-  });
-})();
+  const xDiff = Math.abs(state.playerCircle.position.x - smallCirclePosition.x);
+  const yDiff = Math.abs(state.playerCircle.position.y - smallCirclePosition.y);
+
+  if (Math.sqrt((xDiff^2) + (yDiff^2)) - 10 - state.playerCircle.radius < 0){ // If to catch if the big circle has gotten the small circle
+    score += 1;
+
+    smallCirclePosition = {
+      x : -50,
+      y : -50
+    };
+
+    renderScore();
+  }
+}
+
+window.addEventListener("keydown",function(event){
+  switch (event.code) {
+    case "KeyW":
+
+      state.keysPressed.w = true;
+      break;
+    case "KeyD":
+
+      state.keysPressed.d = true;
+      break;
+    case "KeyA":
+
+      state.keysPressed.a = true;
+  }
+});
+
+window.addEventListener("keyup",function(event){
+  switch (event.code) {
+    case "KeyW":
+
+      state.keysPressed.w = false;
+      break;
+    case "KeyD":
+
+      state.keysPressed.d = false;
+      break;
+    case "KeyA":
+
+      state.keysPressed.a = false;
+  }
+});
+
+window.addEventListener("click",function(event){
+  smallCirclePosition.x = event.clientX - 10
+  smallCirclePosition.y = event.clientY - 25
+});
